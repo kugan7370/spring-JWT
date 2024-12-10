8b9ff4d79d3821f73a4689de2d812d42937f3052
Kugan <kugapriyanj97@gmail.com>
2024-12-11 01:43:56 +0530

    implemented role base route

--- src/main/java/com/kugan/spring_basic/controller/userController.java
+++ src/main/java/com/kugan/spring_basic/controller/userController.java
@@ -2,26 +2,75 @@ package com.kugan.spring_basic.controller;
 
 import com.kugan.spring_basic.entity.User;
 import com.kugan.spring_basic.repository.UserRepository;
+import com.kugan.spring_basic.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
 public class userController {
 
-    private UserRepository userRepository;
+    @Value("${role.admin}")
+    private String roleAdmin;
 
-    public userController(UserRepository userRepository) {
+    @Value("${role.user}")
+    private String roleUser;
+
+    private UserRepository userRepository;
+    private JwtUtil jwtUtil;
+
+    public userController(UserRepository userRepository, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
     }
 
     @GetMapping("/getUser")
     public ResponseEntity<List<User>> getAllUser (){
         List<User> results= userRepository.findAll();
         return ResponseEntity.ok().body(results);
+    }
+
+    @GetMapping("/protected-route")
+    public ResponseEntity<String> getProtectedData(@RequestHeader("Authorization") String token){
+        if(token !=null && token.startsWith("Bearer")){
+            String jwtToken =token.substring(7);
+
+            try{
+                if(jwtUtil.isTokenValid(jwtToken)){
+                    String username = jwtUtil.extractUsername(jwtToken);
+
+                    Set<String> roles =   jwtUtil.extractRoles(jwtToken);
+
+                    if(roles.contains(roleAdmin)){
+                        return ResponseEntity.ok("Welcome " + username + " Here is the " + roles + " specific data");
+
+                    } else if (roles.contains(roleUser)) {
+                        return ResponseEntity.ok("Welcome " + username + " Here is the " + roles + " specific data");
+
+                    }
+                    else{
+                        return ResponseEntity.status(403).body("Access Denied: You don't have the necessary role");
+                    }
+
+
+                }
+
+            } catch (Exception e) {
+               return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid Token");
+            }
+        }
+
+            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authorization header missing or invalid");
+
     }
 }
